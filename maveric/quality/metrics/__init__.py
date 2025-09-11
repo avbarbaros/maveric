ae9fd4d4caff0d2970408f60fff331e2c69cec38
avbarbaros <alivalabarbaros@gmail.com>
2025-09-11 21:48:24 +0300

    BUg fixed about old metric name

--- maveric/quality/metrics/__init__.py
+++ maveric/quality/metrics/__init__.py
@@ -13,7 +13,7 @@ from .semantic_metrics import (
 from .multimodal_metrics import (
     MultimodalConsistencyMetric,
     CrossModalAlignmentMetric,
-    SemanticCaptionGuidedQualityMetric
+    TargetClassQualityMetric
 )
 
 __all__ = [
@@ -22,7 +22,7 @@ __all__ = [
     "ResolutionMetric",
     "SharpnessMetric",
     "ColorDiversityMetric",
-    "SemanticCaptionGuidedQualityMetric",
+    "TargetClassQualityMetric",
     "TextQualityMetric",
     "CaptionLengthMetric",
     "MultimodalConsistencyMetric",
