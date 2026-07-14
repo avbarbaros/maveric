d3ef6e824b4908109387895b944ec54c4e88c064
avbarbaros <alivalabarbaros@gmail.com>
2026-07-14 08:15:20 +0000

    Class name correction for MNIST dataset in the elevater_datasets.py file. The class names have been updated to include both the digit and its corresponding word representation, providing a clearer understanding of the classes in the dataset.

--- maveric/datasets/elevater_datasets.py
+++ maveric/datasets/elevater_datasets.py
@@ -768,16 +768,16 @@ class ELEVATERDataset(BaseDataset):
             'task': 'classification',
             'type': 'file_based',
             'class_names': [
-                '0',
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6',
-                '7',
-                '8',
-                '9'
+                '0 - one',
+                '1 - two',
+                '2 - three',
+                '3 - four',
+                '4 - five',
+                '5 - six',
+                '6 - seven',
+                '7 - eight',
+                '8 - nine',
+                '9 - ten'
             ]
         },
         'oxford_flowers102': {
